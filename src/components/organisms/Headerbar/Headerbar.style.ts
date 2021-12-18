3aab2bde161a393628089ec945e8a1c461bfd741
annie1004619 <annie1004619@naver.com>
2021-12-18 19:29:05 +0900

    feat: add CoursePage UI

--- src/components/organisms/Headerbar/Headerbar.style.ts
+++ src/components/organisms/Headerbar/Headerbar.style.ts
@@ -1,17 +1,24 @@
 import styled, { css } from 'styled-components';
 import { HeaderbarProps } from './Headerbar.type';
+import theme from '@styles/theme';
 
-type HeaderProps = Pick<HeaderbarProps, 'login' | 'scrolled'>;
+type HeaderProps = Pick<
+  HeaderbarProps,
+  'height' | 'login' | 'scrolled' | 'bgColor' | 'color' | 'titleSize'
+>;
+const MAIN = theme.colors.main;
+const WHITE = theme.colors.white;
+const HEADER1 = theme.fontSizes.header1;
 
 export const StyledHeaderbar = styled.div<HeaderProps>`
-  position: fixed;
-  height: 100px;
-  width: calc(100% - 260px);
-  background-color: ${({ theme }) => theme.colors.white};
+  height: ${(props) => props.height || '100px'};
+  width: 100%;
+  background-color: ${(props) => props.bgColor || WHITE};
   color: ${({ theme }) => theme.colors.main};
 
   display: flex;
-  padding: 15px;
+  justify-content: space-between;
+  padding: 15px 3rem;
 
   ${(props) =>
     props.scrolled &&
@@ -26,11 +33,11 @@ export const StyledHeaderbar = styled.div<HeaderProps>`
     `}
 `;
 
-export const StyledTitle = styled.div`
+export const StyledTitle = styled.div<HeaderProps>`
   width: 100%;
-  font-size: ${({ theme }) => theme.fontSizes.header1};
   font-weight: bold;
-  color: ${({ theme }) => theme.colors.main};
+  color: ${(props) => props.color || MAIN};
+  font-size: ${(props) => props.titleSize || HEADER1};
   text-align: left;
 `;
 
@@ -43,8 +50,8 @@ export const StyledSubText = styled.div`
 export const StyledButtonArea = styled.div`
   height: 100%;
   width: 100%;
-  padding: 19px;
   display: flex;
+  align-items: center;
   flex: 1;
   div:nth-child(even) {
     margin: 0px 10px 0px 10px;
